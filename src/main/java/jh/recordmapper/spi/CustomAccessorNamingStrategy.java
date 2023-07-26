0a2d20657540103aec2ad817700b96ff3a114be7
JiaHeng <5684003+jiaheng@users.noreply.github.com>
2023-07-26 11:22:05 +0800

    exclude some method with method name match with certain keywords

--- src/main/java/jh/recordmapper/spi/CustomAccessorNamingStrategy.java
+++ src/main/java/jh/recordmapper/spi/CustomAccessorNamingStrategy.java
@@ -3,6 +3,7 @@ package jh.recordmapper.spi;
 import org.mapstruct.ap.spi.DefaultAccessorNamingStrategy;
 import org.mapstruct.ap.spi.util.IntrospectorUtils;
 
+import java.util.Set;
 import java.util.logging.Logger;
 
 import javax.lang.model.element.ExecutableElement;
@@ -13,40 +14,42 @@ public class CustomAccessorNamingStrategy extends DefaultAccessorNamingStrategy 
     private static final Logger LOG =
             Logger.getLogger(CustomAccessorNamingStrategy.class.getName());
 
+    private static final Set<String> EXCLUDED_GETTER_KEYWORD = Set.of("toString", "hashCode");
+
     @Override
     public boolean isGetterMethod(ExecutableElement method) {
-        return method.getParameters().isEmpty();
+        return super.isGetterMethod(method) || isRecordStyleGetterName(method);
     }
 
     @Override
     public String getPropertyName(ExecutableElement getterOrSetterMethod) {
         if (isRecordStyleGetterName(getterOrSetterMethod)) {
             String methodName = getterOrSetterMethod.getSimpleName().toString();
-            LOG.info("mapping " + methodName + " to " + IntrospectorUtils.decapitalize(methodName));
+            LOG.finest(
+                    () ->
+                            "mapping getter "
+                                    + methodName
+                                    + "() to "
+                                    + IntrospectorUtils.decapitalize(methodName)
+                                    + "() with return type: "
+                                    + getterOrSetterMethod.getReturnType().toString());
             return IntrospectorUtils.decapitalize(methodName);
         }
         return super.getPropertyName(getterOrSetterMethod);
-        //        String methodName = getterOrSetterMethod.getSimpleName().toString();
-        //        return Introspector.decapitalize(methodName);
     }
 
     private boolean isRecordStyleGetterName(ExecutableElement method) {
-        if (!method.getParameters().isEmpty()) {
-            // If the method has parameters it can't be a getter
+        boolean isDefaultGetterMethod = super.isGetterMethod(method);
+        if (isDefaultGetterMethod) {
             return false;
         }
+
+        if (!method.getParameters().isEmpty()
+                || method.getReturnType().getKind() == TypeKind.VOID) {
+            return false;
+        }
+
         String methodName = method.getSimpleName().toString();
-
-        boolean isNonBooleanGetterName =
-                methodName.startsWith("get")
-                        && methodName.length() > 3
-                        && method.getReturnType().getKind() != TypeKind.VOID;
-
-        boolean isBooleanGetterName = methodName.startsWith("is") && methodName.length() > 2;
-        boolean returnTypeIsBoolean =
-                method.getReturnType().getKind() == TypeKind.BOOLEAN
-                        || "java.lang.Boolean".equals(getQualifiedName(method.getReturnType()));
-
-        return !isNonBooleanGetterName && !(isBooleanGetterName && returnTypeIsBoolean);
+        return !EXCLUDED_GETTER_KEYWORD.contains(methodName);
     }
 }
